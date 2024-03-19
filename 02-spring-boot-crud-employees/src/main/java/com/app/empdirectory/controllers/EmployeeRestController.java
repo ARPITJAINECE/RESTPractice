6d9c57bfd95b3ba3fbc22a1423b15b57d5fbad91
unknown <aj241997@gmail.com>
2024-03-19 12:10:59 +0530

    CRUD controllers done

--- 02-spring-boot-crud-employees/src/main/java/com/app/empdirectory/controllers/EmployeeRestController.java
+++ 02-spring-boot-crud-employees/src/main/java/com/app/empdirectory/controllers/EmployeeRestController.java
@@ -2,9 +2,13 @@ package com.app.empdirectory.controllers;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,9 +58,32 @@ public class EmployeeRestController {
 	public Employee saveEmployee(@RequestBody Employee newEmployee) {
 		System.out.println("adding new employee or updating it : " + newEmployee);
 		// we are setting the id to 0, as it is a force to save employee not update
-//		newEmployee.setId(0);
+		newEmployee.setId(0);
 		Employee dbEmployee = employeeServive.save(newEmployee);
 		return dbEmployee;
+	}
+
+	// add mapping for PUT "/employees" updating the existing employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		Employee dbEmployee = employeeServive.save(theEmployee);
+		return dbEmployee;
+	}
+
+	// add mapping for DELETE "/employees/{employeeID}" for deleting a particular
+	// employee
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleteEmpByID(@PathVariable int employeeId) {
+		Employee deleteingEmployee = employeeServive.findById(employeeId);
+
+		// throw exception if employee is not fou8nd!!!
+		if (deleteingEmployee == null) {
+			throw new RuntimeException("employee with the given id is not found : \" + employeeId");
+		}
+
+		employeeServive.deleteById(employeeId);
+
+		return "deleted employee id is : " + employeeId;
 	}
 
 }
