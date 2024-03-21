a82efa755d6e1ff9c0efdae18e4f737944d2508c
unknown <aj241997@gmail.com>
2024-03-21 08:40:47 +0530

    spring data jpa

---
+++ 03-spring-boot-crud-employees-with_spring_jpa/src/main/java/com/app/empdirectory/CrudForEmpDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.app.empdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudForEmpDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudForEmpDirectoryApplication.class, args);
+	}
+
+}
---
+++ 03-spring-boot-crud-employees-with_spring_jpa/src/test/java/com/app/empdirectory/CrudForEmpDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.empdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudForEmpDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
